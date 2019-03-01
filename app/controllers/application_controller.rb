c96fe7050522ef678fc9135cdc677d9e704b3d35
Brent Ransom <sadumedusa@gmail.com>
2019-03-01 20:27:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
+    
+     if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
 
   end
 
@@ -32,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+   user = User.find_by(:username => params[:username])
   end
 
   get "/failure" do
